9da64cf7878d8e42bd2689905ee5d885a43c86ff
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-09 11:36:25 +0530

    Add files via upload

---
+++ PandasEmployee.py
@@ -0,0 +1,45 @@
+# Program to read employee CSV file using Pandas
+
+import pandas as pd
+
+# Read CSV file
+data = pd.read_csv("employees.csv")
+
+# Display data
+print("Employee Data:\n")
+print(data)
+
+# Department-wise average salary
+average_salary = data.groupby("Department")["Salary"].mean()
+
+# Highest salary employee
+highest_salary_employee = data.loc[data["Salary"].idxmax()]
+
+# Display results
+print("\nDepartment-wise Average Salary:\n")
+print(average_salary)
+
+print("\nHighest Salary Employee:\n")
+print(highest_salary_employee)
+
+
+#employees.csv File Content
+
+Name,Department,Salary
+Amit,IT,50000
+Neha,HR,60000
+Rohit,IT,70000
+Priya,Finance,80000
+
+#output:
+Department-wise Average Salary:
+
+Finance    80000
+HR         60000
+IT         60000
+
+Highest Salary Employee:
+
+Name            Priya
+Department    Finance
+Salary          80000
